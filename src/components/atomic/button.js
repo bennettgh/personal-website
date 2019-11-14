8a59e1064bcd5449b9a34a5362f3538b9bf12478
Andrew Bennett <bennetaw@mcmaster.ca>
2019-11-14 07:28:59 -0500

    made a button

---
+++ src/components/atomic/button.js
@@ -0,0 +1,13 @@
+import PropTypes from "prop-types";
+import React from "react";
+import "./atomic.scss";
+
+const Button = ({ text }) => {
+  return (
+    <div className='button'>
+      <span>{text}</span>
+    </div>
+  )
+}
+
+export default Button;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,20 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
+import Button from '../components/atomic/button';
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div className='left'>
-      <h1>Welcome.</h1>
-    </div>
-    <div className='right'>
+    <div id='home' className='split'>
+      <div className='left'>
+        <h1 className='home-title'>Hello,</h1>
+        <h1>I'm Andrew Bennett,</h1>
+        <h1>Full Stack Developer.</h1>
+        <p> ---- Backend <span className='color'>></span> Frontend <span className='color'>></span> Ops</p>
+        <Button text='CONTACT ME'></Button>
+      </div>
+
     </div>
   </Layout>
 );
